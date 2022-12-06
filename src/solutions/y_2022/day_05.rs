23e9a4d3e6424ed711a121f39f8c3288489ae0a0
Niall Flanagan <niall.flanagan@lacework.net>
2022-12-06 13:35:43 +0000

    feat(day05): day 5 solutions
    Signed-off-by: Niall Flanagan <niall.flanagan@lacework.net>

--- src/solutions/y_2022/day_05.rs
+++ src/solutions/y_2022/day_05.rs
@@ -1,18 +1,84 @@
+use std::borrow::BorrowMut;
+
 use crate::Answer;
 
 pub struct Day05;
 
 impl Answer for Day05 {
     fn name(&self) -> &'static str {
-        "?"
+        "Supply Stacks"
     }
 
     fn part_a(&self, input: &str) -> String {
-        "A".to_string()
+        let mut stacks = Vec::from([
+            Vec::from(['Q', 'H', 'C', 'T', 'N', 'S', 'V', 'B']),
+            Vec::from(['G', 'B', 'D', 'W']),
+            Vec::from(['B', 'Q', 'S', 'T', 'R', 'W', 'F']),
+            Vec::from(['N', 'D', 'J', 'Z', 'S', 'W', 'G', 'L']),
+            Vec::from(['F', 'V', 'D', 'P', 'M']),
+            Vec::from(['J', 'W', 'F']),
+            Vec::from(['V', 'J', 'B', 'Q', 'N', 'L']),
+            Vec::from(['N', 'S', 'Q', 'J', 'C', 'R', 'T', 'G']),
+            Vec::from(['M', 'D', 'W', 'C', 'Q', 'S', 'J']),
+        ]);
+        let instructions = input.split("\n").collect::<Vec<&str>>();
+        for instruction in instructions {
+            if instruction.starts_with("move") {
+                // println!("Instruction: {}", instruction);
+                let parts = instruction.split(" ").collect::<Vec<&str>>();
+                let amount = parts[1].parse::<usize>().unwrap();
+                let from = parts[3].parse::<usize>().unwrap() - 1;
+                let to = parts[5].parse::<usize>().unwrap() - 1;
+                for _ in 0..amount {
+                    // println!("Moving {} from {:?} to {:?}", stacks[from][0], stacks[from], stacks[to]);
+                    let val = stacks[from][0];
+                    stacks[from].remove(0);
+                    stacks[to].insert(0, val);
+                }
+                // println!("Stacks: {:?}", stacks);
+            }
+        }
+
+        let code = stacks.iter().map(|s| s[0]).collect::<String>();
+        code
     }
 
     fn part_b(&self, input: &str) -> String {
-        "B".to_string()
+        // let mut stacks = Vec::from([
+        //     Vec::from(['N', 'Z']),
+        //     Vec::from(['D', 'C', 'M']),
+        //     Vec::from(['P']),
+        // ]);
+        let mut stacks = Vec::from([
+            Vec::from(['Q', 'H', 'C', 'T', 'N', 'S', 'V', 'B']),
+            Vec::from(['G', 'B', 'D', 'W']),
+            Vec::from(['B', 'Q', 'S', 'T', 'R', 'W', 'F']),
+            Vec::from(['N', 'D', 'J', 'Z', 'S', 'W', 'G', 'L']),
+            Vec::from(['F', 'V', 'D', 'P', 'M']),
+            Vec::from(['J', 'W', 'F']),
+            Vec::from(['V', 'J', 'B', 'Q', 'N', 'L']),
+            Vec::from(['N', 'S', 'Q', 'J', 'C', 'R', 'T', 'G']),
+            Vec::from(['M', 'D', 'W', 'C', 'Q', 'S', 'J']),
+        ]);
+        let instructions = input.split("\n").collect::<Vec<&str>>();
+        for instruction in instructions {
+            if instruction.starts_with("move") {
+                println!("Instruction: {}", instruction);
+                let parts = instruction.split(" ").collect::<Vec<&str>>();
+                let amount = parts[1].parse::<usize>().unwrap();
+                let from = parts[3].parse::<usize>().unwrap() - 1;
+                let to = parts[5].parse::<usize>().unwrap() - 1;
+
+                let vals = stacks[from][0..amount].to_vec();
+                for val in vals.iter().rev() {
+                    stacks[to].insert(0, *val);
+                    stacks[from].remove(0);
+                }
+                println!("Stacks: {:?}", stacks);
+            }
+        }
+        let code = stacks.iter().map(|s| s[0]).collect::<String>();
+        code
     }
 }
 
@@ -24,12 +90,12 @@ mod tests {
     #[test]
     fn test_part_a() {
         let input = load(2022, 05, "test.txt");
-        assert_eq!("Not A", Day05.part_a(&input))
+        assert_eq!("CMZ", Day05.part_a(&input))
     }
 
     #[test]
     fn test_part_b() {
         let input = load(2022, 05, "test.txt");
-        assert_eq!("Not B", Day05.part_b(&input))
+        assert_eq!("MCD", Day05.part_b(&input))
     }
 }
