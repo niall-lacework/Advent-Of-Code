eea80a4b765cff06f34100c91b5455f9e6d7f708
Niall Flanagan <niall.flanagan@lacework.net>
2022-12-06 14:40:01 +0000

    feat(day06): day 6 solutions
    Signed-off-by: Niall Flanagan <niall.flanagan@lacework.net>

--- src/solutions/y_2022/day_06.rs
+++ src/solutions/y_2022/day_06.rs
@@ -1,18 +1,62 @@
+
+use std::collections::HashSet;
+
 use crate::Answer;
 
 pub struct Day06;
 
 impl Answer for Day06 {
     fn name(&self) -> &'static str {
-        "?"
+        "Tuning Trouble"
     }
 
     fn part_a(&self, input: &str) -> String {
-        "A".to_string()
+        let mut marker: Vec<char> = Vec::new();
+        for (position, character) in input.chars().enumerate() {
+            println!("Marker: {:?}", marker);
+            marker.push(character); // Add the character to the marker
+            if marker.len() > 4 {
+                marker.remove(0);
+            }
+            let mut setv = HashSet::new(); // Create a new set
+            marker.iter().for_each(|x| {
+                setv.insert(x);
+            }); // Add all the characters to the set
+            let mut temp_vec = Vec::new(); // Create a new temp vector because I couldn't get the length of the set?
+            for i in setv {
+                temp_vec.push(i);
+            }
+            println!("Set: {:?}", temp_vec);
+            if temp_vec.len() == 4 {
+                return (position + 1).to_string(); // If the set is 4, return the position
+            }
+        }
+        "No solution found".to_string()
+
     }
 
     fn part_b(&self, input: &str) -> String {
-        "B".to_string()
+        let mut marker: Vec<char> = Vec::new();
+        for (position, character) in input.chars().enumerate() {
+            println!("Marker: {:?}", marker);
+            marker.push(character); // Add the character to the marker
+            if marker.len() > 14 {
+                marker.remove(0);
+            }
+            let mut setv = HashSet::new(); // Create a new set
+            marker.iter().for_each(|x| {
+                setv.insert(x);
+            }); // Add all the characters to the set
+            let mut temp_vec = Vec::new(); // Create a new temp vector because I couldn't get the length of the set?
+            for i in setv {
+                temp_vec.push(i);
+            }
+            println!("Set: {:?}", temp_vec);
+            if temp_vec.len() == 14 {
+                return (position + 1).to_string(); // If the set is 4, return the position
+            }
+        }
+        "No solution found".to_string()
     }
 }
 
@@ -24,12 +68,12 @@ mod tests {
     #[test]
     fn test_part_a() {
         let input = load(2022, 06, "test.txt");
-        assert_eq!("Not A", Day06.part_a(&input))
+        assert_eq!("6", Day06.part_a(&input))
     }
 
     #[test]
     fn test_part_b() {
         let input = load(2022, 06, "test.txt");
-        assert_eq!("Not B", Day06.part_b(&input))
+        assert_eq!("19", Day06.part_b(&input))
     }
 }
